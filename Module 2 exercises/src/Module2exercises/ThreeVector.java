74b056a5d90d8e7ad90d8488982d5bbc03032c02
rbroo <rbroo@DESKTOP-SAB102T>
2017-10-15 22:51:51 +0100

    Finished first versin of Complex class.

--- Module 2 exercises/src/Module2exercises/ThreeVector.java
+++ Module 2 exercises/src/Module2exercises/ThreeVector.java
@@ -16,10 +16,12 @@ public class ThreeVector {
 			return new ThreeVector(x/mag, y/mag, z/mag);
 		else 
 			return new ThreeVector(0,0,0); //Not sure if correct behaviour, nullvector has no direction
+		//Later question in complex (show c1/zero) calls this behaviour choice into question, but in general it is best
+		//to avoid division by zero.
 	}
-	//public String toString() {
-		//return String.valueOf(x) + "i + " + String.valueOf(y) + "j + " + String.valueOf(z) + "k";
-	//}
+	public String toString() {
+		return String.valueOf(x) + "i + " + String.valueOf(y) + "j + " + String.valueOf(z) + "k";
+	}
 	public static double scalarProduct(ThreeVector v1, ThreeVector v2) {
 		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 	}
@@ -42,6 +44,8 @@ public class ThreeVector {
 		if (v1.magnitude() != 0 && v2.magnitude() != 0) //Prevents division by zero
 			return Math.acos(ThreeVector.scalarProduct(v1, v2)/(v1.magnitude() * v2.magnitude()));
 		else return 0; //Not sure if correct behaviour, null vector has no direction to have an angle relative to
+		//Later question in complex (show c1/zero) calls this behaviour choice into question, but in general it is best
+		//to avoid division by zero.
 	}
 	public double notStaticAngle(ThreeVector v1, ThreeVector v2) {
 		return angle(v1, v2);
