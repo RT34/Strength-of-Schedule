518208fe88b5ec95874731337d35e9c5533f5f05
rbroo <rbroo@DESKTOP-SAB102T>
2017-10-16 00:22:32 +0100

    Adding comments on the effects of changing dt.

--- Module 2 exercises/src/Module2exercises/ParticleMain.java
+++ Module 2 exercises/src/Module2exercises/ParticleMain.java
@@ -11,8 +11,9 @@ public class ParticleMain {
 			test.drop(dt[iii]);
 			System.out.println("For dt = " + dt[iii] + ", it took " + test.getT() + " seconds to fall, and reached a speed of " + test.getV() + ".");
 		}
-		
-		
+		System.out.println("As dt decreases, the time to complete fluctuates. This is because of larger, clunkier changes to the v, and thus z, of the " 
+				+ "particle, meaning that it comes closer to finishing at exactly z = 0 the shorter the step. This also causes the changes in final velocity, the"
+				+ " changes in step meaning that a different velocity is achieved just prior to the jump taking the particle past z = 0.");
 	}
 
 }
